1c356ff69a039c6a8242c78a446b84d2bb11297b
mikolaj.przybysz <przybysz.m@pg.com>
2019-04-01 01:45:15 +0200

    init

---
+++ precache-manifest.89a242d3c8adeb92dd938f7d7a9bfe49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bef08e522723c8eb2624",
+    "url": "/static/css/main.46f41415.chunk.css"
+  },
+  {
+    "revision": "bef08e522723c8eb2624",
+    "url": "/static/js/main.bef08e52.chunk.js"
+  },
+  {
+    "revision": "0c48fa3d6951b0d9041e",
+    "url": "/static/js/1.0c48fa3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71a04ffe980617279c88296c1ce57197",
+    "url": "/static/media/bmw.71a04ffe.svg"
+  },
+  {
+    "revision": "30a267a3079d3e4bf40f02cd5f76cce4",
+    "url": "/static/media/bmw_x3.30a267a3.png"
+  },
+  {
+    "revision": "80267cec4ebf9ff0586d4f4ffcb511af",
+    "url": "/index.html"
+  }
+];
